85e707b4578734a3bab52763795b92d05cb53ba2
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2023-03-28 15:06:28 -0300

    Adição dos modelos

--- src/estudos/MyScene.js
+++ src/estudos/MyScene.js
@@ -1,6 +1,9 @@
 import React, { useRef } from 'react';
 import * as THREE from 'three';
 import sky from '../assets/sky.jpg';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import dinoModel from "../assets/tRex.glb";
+import zombieModel from "../assets/Zombie.glb";
 
 function MyScene() {
     const sceneRef = useRef(null);
@@ -17,45 +20,46 @@ function MyScene() {
         renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         document.body.appendChild(renderer.domElement);
 
-        const textureLoader  = new THREE.TextureLoader()
+
+        const loader = new GLTFLoader();
         
-        //Cubo
-        const cubeTexture = textureLoader.load(cubeTextureFile);
-        const cubeGeometry = new THREE.BoxGeometry(3, 3, 3);
-        const cubeMaterial = new THREE.MeshBasicMaterial({
-            map: cubeTexture,
-        });
-        const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        cube.castShadow = true;
+        //Dinossauro
+        loader.load(dinoModel, (glb)=>{
+            glb.scene.scale.set(0.1,0.1,0.1)
+            scene.add(glb.scene)
+        })
+
+        //Zombie
+        loader.load(dinoModel, (glb)=>{
+            glb.scene.scale.set(0.1,0.1,0.1)
+            scene.add(glb.scene)
+        })
 
         //Chão
         const planeGeometry = new THREE.PlaneGeometry(10, 10);
-        const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xebebeb });
+        const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xc2c2c2 });
         const plane = new THREE.Mesh(planeGeometry, planeMaterial);
         plane.rotation.x = -Math.PI / 2;
         plane.position.y = -1;
         plane.receiveShadow = true
 
-        scene.add(cube);
         scene.add(plane);
 
-        //Backround
+        //Background
         const textureLoader = new THREE.TextureLoader();
         const texture = textureLoader.load(sky);
         scene.background = texture;
 
         //Luz branca ambiente
-        const pointLight = new THREE.PointLight(0xffffff, 1, 100);
-        pointLight.position.set(2, 2, 2);
-        pointLight.castShadow = true
-        scene.add(pointLight);
-        
+        const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+        scene.add(ambientLight);
+
         function animate() {
             requestAnimationFrame(animate);
-            
+
             renderer.render(scene, camera);
         }
-        
+
         animate();
     }, []);
 
