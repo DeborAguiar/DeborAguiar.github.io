2578dd0023776cf7ef68336a955fe1ffa714ee34
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2023-03-28 15:37:44 -0300

    Movimentar elementos com o mouse

--- src/estudos/MyScene.js
+++ src/estudos/MyScene.js
@@ -3,6 +3,8 @@ import * as THREE from 'three';
 import sky from '../assets/sky.jpg';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import dinoModel from "../assets/tRex.glb";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls';
 import zombieModel from "../assets/Zombie.glb";
 
 function MyScene() {
@@ -13,25 +15,36 @@ function MyScene() {
         const scene = new THREE.Scene();
 
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 5;
+        camera.position.set(0,3,5);
+        camera.rotation.x -= THREE.MathUtils.degToRad(30);
         const renderer = new THREE.WebGLRenderer({ antialias: true });
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.shadowMap.enabled = true;
         renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         document.body.appendChild(renderer.domElement);
 
-
         const loader = new GLTFLoader();
         
-        //Dinossauro
+        //Controle de camera por orbita
+        //const orbitControls = new OrbitControls(camera, renderer.domElement);
+        
+        //Dinossauro (tem animacao)
         loader.load(dinoModel, (glb)=>{
             glb.scene.scale.set(0.1,0.1,0.1)
+            glb.scene.position.x-=2
+            const transformControls = new TransformControls(camera, renderer.domElement);
+            transformControls.attach(glb.scene)
+            scene.add(transformControls);
             scene.add(glb.scene)
         })
-
-        //Zombie
-        loader.load(dinoModel, (glb)=>{
+        
+        //Zombie (n tem animacao)
+        loader.load(zombieModel, (glb)=>{
             glb.scene.scale.set(0.1,0.1,0.1)
+            glb.scene.position.x+=2
+            const transformControls = new TransformControls(camera, renderer.domElement);
+            transformControls.attach(glb.scene)
+            scene.add(transformControls);
             scene.add(glb.scene)
         })
 
@@ -40,7 +53,6 @@ function MyScene() {
         const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xc2c2c2 });
         const plane = new THREE.Mesh(planeGeometry, planeMaterial);
         plane.rotation.x = -Math.PI / 2;
-        plane.position.y = -1;
         plane.receiveShadow = true
 
         scene.add(plane);
@@ -59,7 +71,6 @@ function MyScene() {
 
             renderer.render(scene, camera);
         }
-
         animate();
     }, []);
 
